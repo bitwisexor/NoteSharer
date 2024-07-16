ad3e723b5465866859e3dc3f3c354b7eaa55271b
bit <AACOSTA179@student.glendale.edu>
2024-07-15 23:03:34 -0700

    Initial Commit.
    Added -filename flag; used to specify which file to upload.

--- notesender.go
+++ notesender.go
@@ -1,1 +1,28 @@
 package ns
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var filename string
+
+func init() {
+	flag.StringVar(&filename, "file", "", "file name")
+	flag.StringVar(&filename, "f", "", "file name (shorthand)")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s -f {filename}\n", os.Args[0])
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if filename == "" {
+		fmt.Println("Error: Filename required.")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
